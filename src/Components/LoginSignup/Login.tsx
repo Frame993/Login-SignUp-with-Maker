bcdd8ac4900a2372045bbcaf539d8711bb70252d
Yordani <yalvarezb93@gmail.com>
2024-03-10 09:47:27 +0300

    First commit

---
+++ src/Components/LoginSignup/Login.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Login() {
+  return (
+    <div className="container">
+      <h1>Welcome Back</h1>
+      <p>Please login to your account</p>
+      <div className="buttons-container">
+      <button>Google</button>
+      <button>faceboo</button>
+      </div>
+    </div>
+  )
+}
+
